43cdc551e20e62ee30164526c42fedb4e1136ef2
benjamin <912249151@qq.com>
2019-08-26 16:14:12 +0800

    Initial commit

---
+++ src/main/java/com/group1/webshop/WebshopApplication.java
@@ -0,0 +1,13 @@
+package com.group1.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopApplication.class, args);
+    }
+
+}

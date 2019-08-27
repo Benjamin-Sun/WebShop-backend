f449ea590bae34fe057215b6313c2deb759548fe
benjamin <912249151@qq.com>
2019-08-27 22:14:49 +0800

    添加所有模块

---
+++ webshop-ops/src/main/java/com/group1/ops/OpsApplication.java
@@ -0,0 +1,13 @@
+package com.group1.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpsApplication.class, args);
+    }
+
+}
